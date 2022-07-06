9db1b4459949593b81588c817d9f03312bd8caf4
Stephen <stephen@windscribe.com>
2022-07-06 17:00:58 -0400

    Dark and light themes

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../../assets/img/logo.svg'
+import './Popup.css'
+import { ThemeProvider } from 'theme-ui'
+import { theme } from '../../theme'
+
+const Popup = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React!
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default Popup
