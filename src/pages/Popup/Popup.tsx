4162a6ebea9a4171450dd13b46d4201cea1bba5e
Stephen <stephen@windscribe.com>
2022-07-07 09:51:08 -0400

    Removed unsorted colors

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from '../../assets/img/logo.svg'
 import './Popup.css'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '../../theme'
@@ -9,7 +8,6 @@ const Popup = () => {
     <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
           </p>
