96babfb93be5294cf4c5ae7488a7a6e5b2a4c167
Roman Savarin <roman.s@windscribe.com>
2022-07-21 15:16:08 +0300

    Fixes after merging master branch

--- src/views/SplashPage/index.tsx
+++ src/views/SplashPage/index.tsx
@@ -1,81 +1,73 @@
-// import React from 'react';
-// import { Flex } from 'rebass';
-// // import { useTheme, useDispatch, useConnect } from 'ui/hooks';
-// // import { useTranslation } from 'react-i18next';
-// // import { ThemeContext } from '@emotion/core';
-// // import websiteLink from 'utils/websiteLink';
-// import Header from './Header';
-// // import { SimpleButton } from 'ui/Button';
-// // import styled from '@emotion/styled';
-// // import { actions } from 'state';
-// // import LoadingScreen from 'components/LoadingScreen';
-// import splashBG from 'assets/splash/desksplashpng.png';
+import { Button, Flex, Text, useThemeUI } from 'theme-ui';
+import splashBackground from 'assets/img/splashBackground.png';
+import rotatingLogo from 'assets/img/rotatingLogo.gif';
 
-// const GetStartedButton = styled(SimpleButton)`
-//   margin: 52px auto 0px;
-//   padding: 9px 54px;
-//   color: #020d1c;
-//   background-color: #55ff8a;
-//   border: none;
-//   border-radius: 20px;
-//   height: 40px;
-//   width: 228px;
-//   font-size: 14px;
-//   &:hover {
-//     background-color: white;
-//   }
-// `;
+const SplashPage = () => {
+	const { theme } = useThemeUI();
+	return (
+		<Flex
+			sx={{
+				flexDirection: 'column',
+				alignItems: 'center',
+				position: 'relative',
+				backgroundImage: `url(${splashBackground})`,
+				backgroundRepeat: 'no-repeat',
+				height: '100vh',
+			}}
+		>
+			<img
+				src={rotatingLogo}
+				alt="logo"
+				sx={{
+					mt: '48px',
+					width: '40px',
+					height: '40px',
+				}}
+			/>
+			<Text
+				sx={{
+					mt: '24px',
+					fontSize: '24px',
+					fontWeight: '600',
+					color: theme.colors?.white,
+				}}
+			>
+				Keep Your Secrets.
+			</Text>
+			<Button
+				variant="rectangle"
+				sx={{
+					mt: '52px',
+					fontSize: '14px',
+					width: '228px',
+					height: '40px',
+					color: theme.colors?.black,
+					backgroundColor: theme.colors?.green,
+					textAlign: 'center',
+					':hover': {
+						backgroundColor: theme.colors?.white,
+					},
+				}}
+			>
+				Get Started
+			</Button>
+			<Button
+				variant="simple"
+				sx={{
+					mt: '24px',
+					opacity: '0.5',
+					fontSize: '14px',
+					fontWeight: '600',
+					color: theme.colors?.white,
+					':hover': {
+						opacity: '1',
+					},
+				}}
+			>
+				Login
+			</Button>
+		</Flex>
+	);
+};
 
-// // const LoginButton = styled(SimpleButton)`
-// //   width: 50%;
-// //   color: ${({ theme }) => theme.colors.white};
-// //   opacity: 0.5;
-// //   &:hover {
-// //     opacity: 1;
-// //   }
-// //   font-size: ${({ theme }) => theme.fontSizes[1]};
-// //   font-weight: ${({ theme }) => theme.fontWeights[1]};
-// //   margin: 25px auto;
-// // `;
-
-// const SplashPage = () => {
-//   // const { colors } = useTheme(ThemeContext);
-//   // const dispatch = useDispatch();
-//   // const { t } = useTranslation();
-//   // const loggingIn = useConnect((s) => s.loggingIn);
-//   // const ghostAccount = () => dispatch(actions.ghost.login());
-//   // const loginClick = () => dispatch(actions.view.set('Login'));
-
-//   return (
-//     // <>
-//     //   {loggingIn ? (
-//     //     <LoadingScreen text={t('Logging you in...')} />
-//     //   ) : (
-//     //     <>
-//     <Flex
-//       color={colors.white}
-//       sx={{
-//         flexDirection: 'column',
-//         alignItems: 'center',
-//         position: 'relative',
-//         backgroundImage: `url(${splashbg})`,
-//         backgroundRepeat: 'no-repeat',
-//         backgroundSize: '324px 300px',
-//         // minHeight: '300px',
-//       }}
-//     >
-//       <Header home={home} />
-//       <GetStartedButton name="ghost" onClick={ghostAccount}>
-//         {t('Get Started')}
-//       </GetStartedButton>
-//       <LoginButton name="login" onClick={loginClick}>
-//         {t('Login')}
-//       </LoginButton>
-//     </Flex>
-//     //     </>
-//     //   )}
-//     // </>
-//   );
-// };
-
-export default () => <p>SplashPage</p>;
+export default SplashPage;
