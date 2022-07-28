42fb3adab7eaa17940a3013b946208a4f48c6929
Stephen <stephen@windscribe.com>
2022-07-28 16:22:02 -0400

    Clean up

--- src/state/slices/view.ts
+++ src/state/slices/view.ts
@@ -11,7 +11,7 @@ export interface ViewState {
 
 const defaultState: ViewState = {
   previous: [],
-  current: 'Login',
+  current: 'SplashPage',
 }
 
 export const create: Create<ViewState> = initialState =>
