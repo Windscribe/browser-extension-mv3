63d08f87bbc7191a50a399cee4c7776df1ec6d8b
Roman Savarin <roman.s@windscribe.com>
2022-08-03 19:03:23 +0300

    Delete Example page

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,3 @@
-export { default as Example } from './Example/Example'
 export { default as SplashPage } from './SplashPage/SplashPage'
 export { default as Signup } from './Signup/Signup'
 export { default as Login } from './Login/Login'
