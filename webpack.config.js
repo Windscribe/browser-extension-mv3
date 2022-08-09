75ea62c645dd66d7cf355a64373ed23f89bcecf7
Roman Savarin <roman.s@windscribe.com>
2022-08-09 13:50:57 +0300

    Add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const alias = {
   'react-dom': '@hot-loader/react-dom',
 }
-
+console.log('env', env.NODE_ENV)
 // load the secrets
 const secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js')
 
