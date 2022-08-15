fd41c2bac0fc7ef6098c33b599527a92d7d7feab
Roman Savarin <roman.s@windscribe.com>
2022-08-15 17:19:14 +0300

    Remove Tippy

--- src/utils/addOneMonthToDate.ts
+++
@@ -1,8 +0,0 @@
-import { addMonths, format, parse } from 'date-fns'
-
-export default date => {
-  // adds 1 month to resetDate
-  const dateObj = parse(date)
-  const nextResetDateObj = addMonths(dateObj, 1)
-  return format(nextResetDateObj, 'YYYY-MM-DD')
-}
