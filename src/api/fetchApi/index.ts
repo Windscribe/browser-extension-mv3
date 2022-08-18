e01c5bcb39589d381d0104d740cf504867c30683
Stephen <stephen@windscribe.com>
2022-08-18 13:08:10 -0400

    rebase fixes

--- src/api/fetchApi/index.ts
+++ src/api/fetchApi/index.ts
@@ -1,9 +1,6 @@
 import { type ApiResponse, Method } from 'api/types'
 
 const fetchApi = async (path: string, method: Method, useAssets: boolean, useBackup = false) => {
-import { type ApiResponse, method } from 'api/types'
-
-const fetchApi = async (path: string, method: method, useAssets: boolean, useBackup = false) => {
   let url: string
 
   // Change url's when using actually api
