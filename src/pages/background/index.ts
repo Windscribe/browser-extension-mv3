ba2a673dd72cb7d85708163d49644ff8b97dd3ef
Roman Savarin <roman.s@windscribe.com>
2022-09-19 19:16:12 +0300

    Update webext-redux and get rid of workaround

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,14 +1,5 @@
 import log from 'utils/log'
-import browserApi from 'services/browserApi'
 import { initializeWrappedStore } from 'state'
-import { WAKE_UP_NEO } from 'utils/constants'
-
-// TODO consider if it is still needed
-browserApi.runtime.onMessage.addListener((request, _, sendResponse) => {
-  if (request.type === WAKE_UP_NEO) {
-    sendResponse({ type: 'background-service-worker-is-running' })
-  }
-})
 
 initializeWrappedStore().then(() => {
   log('bg store was initialized')
