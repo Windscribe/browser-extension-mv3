5a87b2b09a659c28d761a09deff01bec20af6b58
Roman Savarin <roman.s@windscribe.com>
2022-09-27 12:24:38 +0300

    Rename serverList to getServerList

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
+import { getServerList } from './serverList'
+
 export { default as login } from './login'
 export { default as sessionStatus } from './sessionStatus'
 export { default as notifications } from './notifications'
 export { default as bestLocation } from './bestLocation'
 export { default as serverCredentials } from './serverCredentials'
-export { default as serverList } from './serverList'
+export { getServerList }
