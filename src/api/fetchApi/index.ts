ea9dea0d96a27655ce6f1cc1b579b4d4cdcd66eb
Roman Savarin <roman.s@windscribe.com>
2022-10-21 22:41:44 +0300

    Choose apiUrl depend on env

--- src/api/fetchApi/index.ts
+++ src/api/fetchApi/index.ts
@@ -1,5 +1,5 @@
 import type { ApiResponse, Method } from 'api/types'
-import { ENVS } from 'utils/constants'
+import { ENVS, NODE_ENV } from 'utils/constants'
 
 let workingApi: string | null = null
 
@@ -32,7 +32,14 @@ const fetchApi = async (apiUrl: string, path: string, method: Method, useAssets:
     workingApi = apiUrl
   }
 
-  const url = useAssets ? `assets.${apiUrl}` : `api.${apiUrl}`
+  let url: string
+  if (NODE_ENV === 'production') {
+    url = useAssets ? `assets.${apiUrl}` : `api.${apiUrl}`
+  } else {
+    url = useAssets ? `assets-${apiUrl}` : `api-${apiUrl}`
+  }
+  console.log('url: ', url)
+  console.log('NODE_ENV: ', NODE_ENV)
 
   return fetchTimeout(`https://${url}/${path}`, method)
 }
@@ -42,10 +49,17 @@ const sendRequest = async <DataType>(
   method: Method,
   useAssets = false,
 ): Promise<ApiResponse<DataType>> => {
-  return await fetchApi(workingApi || ENVS.API_URL, path, method, useAssets)
+  console.log('ENVS.API_URL: ', ENVS.API_URL)
+
+  workingApi ??= ENVS.API_URL
+
+  return await fetchApi(workingApi, path, method, useAssets)
     .then(response => response.json())
     .catch(async () => {
       workingApi = null
+      if (!ENVS.BACKUP_API_URL) {
+        throw Error('API connectivity issues')
+      }
       return await fetchApi(ENVS.BACKUP_API_URL, path, method, useAssets)
         .then(response => response.json())
         .catch(async () => {
