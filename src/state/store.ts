d9c403388df6130ad30311d867b8c1ef7ea90b37
Roman Savarin <roman.s@windscribe.com>
2022-12-19 14:45:08 +0300

    Create debugLog middleware

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,14 @@
-import { configureStore, combineReducers, type AnyAction } from '@reduxjs/toolkit'
+import {
+  configureStore,
+  combineReducers,
+  type AnyAction,
+  type Dispatch,
+  type Middleware,
+} from '@reduxjs/toolkit'
 import { createLogger } from 'redux-logger'
 import { alias } from '@eduardoac-skimlinks/webext-redux'
 
+import { LogTag, LogItem } from 'utils/types'
 import aliases from './aliases'
 import viewReducer from './slices/view'
 import proxyReducer from './slices/proxy'
@@ -9,7 +16,7 @@ import blockerReducer from './slices/blocker'
 import sessionReducer from './slices/session'
 import serversReducer from './slices/servers'
 import newsfeedReducer from './slices/newsfeed'
-import debugLogReducer from './slices/debugLog'
+import debugLogReducer, { pushToDebugLog } from './slices/debugLog'
 import whitelistReducer from './slices/whitelist'
 import autopilotReducer from './slices/autopilot'
 import connectionReducer from './slices/connection'
@@ -53,19 +60,36 @@ const store = configureStore({
   reducer: rootReducer,
 })
 
-const logger = createLogger({
-  collapsed: (getState, action, logEntry) => !logEntry?.error,
+const consoleLogger = createLogger({
+  collapsed: (_, action, logEntry) => !logEntry?.error,
+  predicate: (_, action) => action.type !== pushToDebugLog.type,
 })
+
+const debugLogMiddleware: Middleware<Dispatch, RootState> = store => next => action => {
+  if (action.type !== pushToDebugLog.type) {
+    const message = `Redux action: ${action.type}`
+
+    let tag: LogTag = 'background'
+    if (action._sender?.url.includes('popup.html')) tag = 'popup'
+    if (action._sender?.url.includes('debugLog.html')) tag = 'debugLog'
+    // Need a check if(contentScript) When contentScript will be added
+
+    const logItem: LogItem = { message, tag }
+    if (action.payload) logItem.data = action.payload
+    store.dispatch(pushToDebugLog(logItem))
+  }
+  return next(action)
+}
 
 export function buildFrom(preloadedState?: RootState): StoreType {
   return configureStore({
     reducer: rootReducer,
     preloadedState,
     middleware: getDefaultMiddleware => {
-      const arr = [alias(aliases), ...getDefaultMiddleware()]
-      // if (process.env.NODE_ENV === 'development') {
-      arr.push(logger)
-      //}
+      const arr = [debugLogMiddleware, alias(aliases), ...getDefaultMiddleware()]
+      if (process.env.NODE_ENV === 'development') {
+        arr.push(consoleLogger)
+      }
       return arr
     },
   })
