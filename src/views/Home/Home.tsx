ce9c1ff19ff3f300e1233631b843915e89976143
z0ccc <z0ccc@proton.me>
2023-05-04 11:39:23 -0400

    ublock detection fixes

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -62,8 +62,8 @@ const Home: ThemeUiElement = () => {
     if (isRightAfterLogin) {
       dispatch(setIsRightAfterLogin(false))
       detectUblock().then(isUblockInstalled => {
-        isUblockInstalled && dispatch(addOverlay('ublockDetected'))
         dispatch(addOverlay('welcome'))
+        isUblockInstalled && dispatch(addOverlay('ublockDetected'))
       })
     }
   }, [isRightAfterLogin, dispatch])
