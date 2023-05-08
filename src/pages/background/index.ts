7ef508e140e9d60f2953113a8e6e6efdb78dfa68
Roman Savarin <roman.s@windscribe.com>
2023-05-08 15:41:52 +0200

    Create session change listener

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -15,8 +15,6 @@ import type { WorkerNavigatorWithConnection } from 'utils/navigatorNetworkInform
 import { checkSessionStatus } from 'state/slices/session'
 import { chooseIcon } from 'state/slices/iconVariant'
 import { fetchNotifications } from 'state/slices/newsfeed'
-import { fetchServerList } from 'state/slices/servers'
-import { fetchServerCredentials } from 'state/slices/serverCredentials'
 
 const bgStore = initializeWrappedStore().then(store => {
   store.dispatch(pushToDebugLog({ message: 'Bg store was initialized', tag: 'background' }))
@@ -60,39 +58,14 @@ chrome.alarms.create('sessionPoller', { periodInMinutes: 10 })
 chrome.alarms.create('notificationPoller', { periodInMinutes: 720 })
 
 chrome.alarms.onAlarm.addListener(async alarm => {
+  const store = await bgStore
   if (alarm.name === 'sessionPoller') {
-    const store = await bgStore
-
-    const oldSession = store.getState().session
     await store.dispatch(checkSessionStatus())
-    const newSession = store.getState().session
-
-    const sessionDoNotCompareArr = [
-      'email',
-      'email_status',
-      'our_ip',
-      'reg_date',
-      'traffic_used',
-      'user_id',
-      'username',
-      'loading',
-    ] as Array<keyof typeof oldSession>
-
-    const oldSessionCompare = Object.assign({}, oldSession)
-    const newSessionCompare = Object.assign({}, newSession)
-
-    for (const property of sessionDoNotCompareArr) {
-      delete oldSessionCompare[property]
-      delete newSessionCompare[property]
-    }
-
-    if (JSON.stringify(oldSessionCompare) !== JSON.stringify(newSessionCompare)) {
-      await store.dispatch(fetchServerCredentials())
-      store.dispatch(fetchServerList())
-    }
-  } else if (alarm.name === 'notificationPoller') {
-    const store = await bgStore
-    store.dispatch(fetchNotifications())
+    return
+  }
+  if (alarm.name === 'notificationPoller') {
+    await store.dispatch(fetchNotifications())
+    return
   }
 })
 
