10c7638901d6f8d7d1448d06c67e303553259e2c
Roman Savarin <roman.s@windscribe.com>
2023-07-13 11:15:37 +0200

    Add sort by date for Newsfeed

--- src/views/Newsfeed/Newsfeed.tsx
+++ src/views/Newsfeed/Newsfeed.tsx
@@ -30,14 +30,16 @@ const Newsfeed: ThemeUiElement = () => {
     <Box data-testid="newsfeed-page" bg="background">
       <Header title="News Feed" />
       <ScrollableBox>
-        {notifications.map(({ id, date, title, message }) => (
-          <NewsfeedItem
-            key={id}
-            isViewed={viewedNewsIds.includes(id)}
-            isExpanded={expandedId === id}
-            {...{ id, date, title, message, handleItemClick }}
-          />
-        ))}
+        {notifications
+          .sort((a, b) => b.date - a.date)
+          .map(({ id, date, title, message }) => (
+            <NewsfeedItem
+              key={id}
+              isViewed={viewedNewsIds.includes(id)}
+              isExpanded={expandedId === id}
+              {...{ id, date, title, message, handleItemClick }}
+            />
+          ))}
       </ScrollableBox>
     </Box>
   )
