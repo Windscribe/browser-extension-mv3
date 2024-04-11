6d3477df708f925a47f1433f107c2d75106e6082
Ahmed Rafi Ullah <ahmed@windscribe.com>
2024-04-11 11:00:35 -0400

    chore: move migration runner to onInstalled handler

--- src/pages/background/main.ts
+++ src/pages/background/main.ts
@@ -13,8 +13,6 @@ import {
   messageHandler,
 } from './eventHandlers'
 
-import { runMigrationFromManifestV2ToV3 } from 'migrations/v2ToV3migration'
-
 declare const self: ServiceWorkerGlobalScope
 
 try {
@@ -24,11 +22,6 @@ try {
     store.dispatch(chooseIcon())
     return store
   })
-
-  ;(async () => {
-    const store = await bgStore
-    await runMigrationFromManifestV2ToV3(store)
-  })()
 
   chrome.runtime.onInstalled.addListener(onInstalledHandler(bgStore))
 
