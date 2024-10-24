753fe24017850f668b820ed4c052311b2a583046
Ahmed Rafi Ullah <ahmed@windscribe.com>
2024-10-24 17:29:14 -0400

    chore: fix bug

--- src/pages/background/eventHandlers/permissionsHandler.ts
+++ src/pages/background/eventHandlers/permissionsHandler.ts
@@ -1,15 +1,30 @@
 import { StoreType } from 'state'
+import {
+  enableBlockNotifications,
+  resetNotificationBlocker,
+} from 'state/slices/notificationBlockerEnabled'
 import { removeOverlay } from 'state/slices/overlay'
 import { addPermissions, removePermissions } from 'state/slices/permissions'
+import { setShouldShowReloadAlert } from 'state/slices/reloadAlert'
 import { CONTENT_SETTINGS } from 'utils/constants'
 
 export function handlePermissionsAdded(bgStore: Promise<StoreType>) {
   return async (permissions: chrome.permissions.Permissions): Promise<void> => {
     const store = await bgStore
+
+    // content settings permissions for notification blocker
     if (permissions.permissions?.includes(CONTENT_SETTINGS)) {
+      const isNotificationBlockerEnabled = store.getState().notificationBlockerEnabled
       store.dispatch(addPermissions([CONTENT_SETTINGS]))
       // close it here since the native modal closes the popup
       store.dispatch(removeOverlay('notificationBlockerPermission'))
+      if (isNotificationBlockerEnabled) {
+        store.dispatch(enableBlockNotifications())
+      } else {
+        store.dispatch(resetNotificationBlocker())
+      }
+
+      store.dispatch(setShouldShowReloadAlert(true))
     }
   }
 }
