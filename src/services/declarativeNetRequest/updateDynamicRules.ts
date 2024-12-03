872ad2bca80025bb9f239712db37d915f5e20632
Ahmed Rafi Ullah <ahmed@windscribe.com>
2024-12-03 18:20:44 -0500

    fix: remove list header value

--- src/services/declarativeNetRequest/updateDynamicRules.ts
+++ src/services/declarativeNetRequest/updateDynamicRules.ts
@@ -66,7 +66,6 @@ const spoofUserAgentHeaderRuleTemplate: chrome.declarativeNetRequest.Rule = {
       {
         header: 'sec-ch-ua-full-version-list',
         operation: chrome.declarativeNetRequest.HeaderOperation.REMOVE,
-        // value: '',
       },
       {
         header: 'sec-ch-ua-mobile',
